499df9f814ea3f6f76daee6f7d989234643861ec
Harshvdev <harsh.vs.tech@gmail.com>
2025-04-24 10:50:39 +0530

    Add Day-01

---
+++ Day-01/practice.py
@@ -0,0 +1,20 @@
+#Day-1
+print("Hello World!")
+
+
+print("     /|")
+print("    / |")
+print("   /  |")
+print("  /   |")
+print(" /    |")
+print("/_____|")
+
+
+
+name = "Harsh"
+age = "18"
+
+print(f"{name} is a good person (XD), {name} is {age} years old. He loves chocolates but hates Dal! {name} will turn {age+1} years old after one year. He will become an old man.")
+
+
+print(name+ " is a good person. "+name+" is "+age+" years old.")
