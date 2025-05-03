07ed39142d5e503c514b2d42e547bd08b761bad2
Harshvdev <harsh.vs.tech@gmail.com>
2025-05-03 21:31:28 +0530

    Update learn_random.py

--- Day-13/learn_random.py
+++ Day-13/learn_random.py
@@ -10,7 +10,16 @@ print(random.randrange(0, 11, 2)) #Returns a number from the range [start, stop)
 
 test_list = ['Apple', 'Banana', 'Tomato', 'Grapes']
 print(random.choice(test_list)) #Returns a random item from the list.
-print(random.choices(test_list, k=2)) #
+print(random.choices(test_list, k=2)) #Select 2 items with replacemen
+
+items = [1, 2, 3, 4, 5]
+random.shuffle(items) # Shuffles the list in place
+print(items)
+
+random.seed(1) # Set a seed to reproduce the same random results
+print(random.randint(1, 100)) #Now it will produce 18 every time because of seed function above it.
+
+
 
 
 
