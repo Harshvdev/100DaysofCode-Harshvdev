96a5045b6c12560d8c31538b5c4342f680476717
Harshvdev <harsh.vs.tech@gmail.com>
2025-06-04 23:41:32 +0530

    Learned more Js string operations

---
+++ Day-37/README.md
@@ -0,0 +1,11 @@
+**Day 37 – #100DaysofCode**
+
+Practiced core JavaScript string methods today:
+
+• slice(), split(), replace(), repeat()
+• toUpperCase(), padStart(), padEnd()
+• Accessing characters with [] and charAt()
+
+Coming from Python, I still wonder why we have to write .toUpperCase() instead of something simpler like .upper() 🙃
+
+4 June 2025 - 11:40 PM
---
+++ Day-37/script.js
@@ -0,0 +1,20 @@
+let name = "Harsh";
+let first = name[0];
+alert(first);
+let txt = "Apple, Banana, Coffee";
+let part = txt.slice(7, 13);
+alert(part);
+alert(name.toUpperCase());
+let example = "✨Tada✨"
+let paddedStart = example.padStart(8, "0");
+let paddedEnd = example.padEnd(8, "0");
+alert(paddedStart);
+alert(paddedEnd);
+let repeattext = "Hehe";
+let repeated = repeattext.repeat(2)
+alert(repeated)
+let onetext = "Please visit One Piece."
+let newonetext = onetext.replace("One", "Two")
+alert(newonetext)
+onetext = onetext.split(" ")
+alert(onetext)
