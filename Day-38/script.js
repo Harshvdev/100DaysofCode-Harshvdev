aaeb98fb950ae3118e605a48baf4668dceb6fabd
Harshvdev <harsh.vs.tech@gmail.com>
2025-06-05 23:57:21 +0530

    Practiced Js if-else

---
+++ Day-38/script.js
@@ -0,0 +1,10 @@
+if (2 < 3) {
+  alert(2<3); // true
+}
+
+let first_name = "Harsh";
+if (first_name === "Harsh") {
+  alert(`Yep, your name is ${first_name}.`); // Yep, your name is Harsh.
+} else {
+  alert("Nah, you're not Harsh.");
+}
